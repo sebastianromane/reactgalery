db87e0a8d1552830b1d03604155d840a3c088e2e
sebastianromane <sebastianromane@gmail.com>
2022-08-20 00:02:34 -0400

    Insert html structure to React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// Insert html structure to React
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fondo">
+      
+      <div className="head">
+      <img className="imgGif" src="https://rb.gy/uvajsp" alt="homeMovingGif"></img>
+      <Header title="Galery of your Home with React" />
+      </div>
+      
+
+      <div className="container">
+        <div className="galeria">
+          <Cards
+            image="https://rb.gy/sob25h"
+            title="Modelo 1"
+            description="Casa campestre"
+          />
+        </div>
+
+        <div className="galeria">
+          <Cards
+            image="https://rb.gy/binarr"
+            title="Modelo 2"
+            description="Casa citadina"
+          />
+        </div>
+
+        <div className="galeria">
+          <Cards
+            image="https://rb.gy/gja7vd"
+            title="Modelo 3"
+            description="Casa tipo granja"
+          />
+        </div>
+      </div>
+
+               
+      <Footer footer="© 2022 Copyright Desafío Latam:" button="More Info." />
     </div>
   );
 }
-
 export default App;
